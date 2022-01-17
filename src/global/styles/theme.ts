1b4f535e70bbe63e87ab50a915af340c23f8ba0d
mbssilva <mbernardi.s@outlook.com>
2022-01-16 21:56:46 -0300

    Configura os temas da aplicação com styled-components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: '#5636d3',
+
+    secundary: '#ff872c',
+    secondaty_dark: 'rgba(255, 135, 44, 0.3)',
+
+    success: '#12a454',
+    success_dark: 'rgba(18, 164, 84, 0.9)',
+
+    attention: '#e83f5b',
+    attention_dark: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#fff',
+    title: '#363f5f',
+    text: '#969cb2',
+    background: '#f0f2f5'
+  },
+}
