98f161ffe0768ce06bda7518a3478ac206d6e990
mbssilva <mbernardi.s@outlook.com>
2022-02-20 15:54:54 -0300

    Deleta componente duplicado

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -8,7 +8,7 @@ interface CategoryProps {
 }
 
 
-export const Container = styled(GestureHandlerRootView)`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
 `;
